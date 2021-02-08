413c89bbc33de7cdb1fea3b311c8a2baaeb0f0a3
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-02-09 01:23:55 +0200

    Connected bootstrap(full). Added navbar template.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
-
+const jquery = require('./plugins/jquery')
+const webpack = require('webpack');
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}));
+const config = environment.toWebpackConfig();
+config.resolve.alias = {
+    jquery: 'jquery/src/jquery'
+};
+environment.plugins.prepend('jquery', jquery)
 module.exports = environment
