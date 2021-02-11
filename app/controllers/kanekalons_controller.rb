77cf73079b1660020a8e0106fa8f13d59230007c
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-02-11 19:06:11 +0200

    Added logic to choose and mix colors

--- app/controllers/kanekalons_controller.rb
+++ app/controllers/kanekalons_controller.rb
@@ -1,5 +1,31 @@
 class KanekalonsController < ApplicationController
+
   def index
     @kanekalons = Kanekalon.all
+    @result = if params[:color_name] == "Red"
+                result_color(params[:color_name])
+                if params[:manufacture] == "Kami"
+                  "600 90% + 300 10%"
+                elsif params[:manufacture] == "Jumbo"
+                  "Jumbo"
+                else
+                  "Красный + Белый для осветления, жёлтый, чтобы получить оранжево-красный"
+                end
+              elsif params[:color_name] == "Green"
+                result_color(params[:color_name])
+                if params[:manufacture] == "Kami"
+                  "300 90% + 300 10%"
+                elsif params[:manufacture] == "Jumbo"
+                  "Jumbo"
+                else
+                  'Жёлтый + добавить синий и зелёный'
+                end
+              end
+  end
+
+  private
+
+  def result_color(color)
+    @result_color = Kanekalon.where(color_name: color).take
   end
 end
