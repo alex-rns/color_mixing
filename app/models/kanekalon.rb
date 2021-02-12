2400bd88973d10c962c17bb35bf85b5b334de3d3
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-02-13 00:54:08 +0200

    Styled nav, added annotate to model

--- app/models/kanekalon.rb
+++ app/models/kanekalon.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: kanekalons
+#
+#  id           :bigint           not null, primary key
+#  hex          :string
+#  color_name   :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  default_mix  :string
+#  kami         :string
+#  jumbo_braids :string
+#  aida         :string
+#
 class Kanekalon < ApplicationRecord
 end
--- test/models/kanekalon_test.rb
+++ test/models/kanekalon_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: kanekalons
+#
+#  id           :bigint           not null, primary key
+#  hex          :string
+#  color_name   :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  default_mix  :string
+#  kami         :string
+#  jumbo_braids :string
+#  aida         :string
+#
 require "test_helper"
 
 class KanekalonTest < ActiveSupport::TestCase
